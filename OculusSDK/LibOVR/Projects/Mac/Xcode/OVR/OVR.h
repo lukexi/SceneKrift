740c2b191475e2c51a18ffabafdc7a402652d8b8
Luke Iannini <lukexi@me.com>
2014-08-31 18:38:50 -0700

    Stray files

---
+++ OculusSDK/LibOVR/Projects/Mac/Xcode/OVR/OVR.h
@@ -0,0 +1,22 @@
+//
+//  OVR.h
+//  OVR
+//
+//  Created by Luke Iannini on 8/24/14.
+//  Copyright (c) 2014 Oculus VR Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OVR.
+FOUNDATION_EXPORT double OVRVersionNumber;
+
+//! Project version string for OVR.
+FOUNDATION_EXPORT const unsigned char OVRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OVR/PublicHeader.h>
+
+
+#import <OVR/OVR_CAPI.h>
+#import <OVR/OVR_Math.h>
+#import <OVR/OVR_Version.h>
